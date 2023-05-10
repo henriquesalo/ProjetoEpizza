a5e53c256782ba4fd4ba9e7ef249ccd8dee00b80
VieiraGui147 <77821542+Vieirinhagui@users.noreply.github.com>
2023-05-10 20:48:59 -0300

    commit final, trocando todos os arquivos

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+
+document.getElementById("menu-hamburguer").onclick = function() {
+    document.getElementById("background").style.display = "flex";
+}
+document.getElementById("icon-fechar").onclick = function() {
+    document.getElementById("background").style.display = "none";
+}
